afefcc66d5d013742b45f072fbee59b25ffe9bd8
Jan Chyczynski <jasiek.ch16@gmail.com>
2023-11-11 20:58:23 +0100

    LHCInfo* validation tools: exeption: Assertion `infoPerLS.fillNumber() == 7066' failed
    add dump_LHCInfoPer_payloads.py
    
    add ppsLHCInfoPerLSComp.py
    
    rename dumpLHCInfoPerPayloads.py and compareLHCInfosToPPSDB.py
    
    renaming/moving files, printLHCInfoPerPayloads argument validation
    
    disable unecessary logs from LHCInfoPerFillAnalyzer
    
    Add lumiid timetype support for LHCInfoPerLS printing
    
    Validation: add lumiid support for LHCInfoPerFill printing

--- CondTools/RunInfo/test/LHCInfoPerFillAnalyzer_cfg.py
+++ CondTools/RunInfo/test/LHCInfoPerFillAnalyzer_cfg.py
@@ -1,6 +1,40 @@
 import FWCore.ParameterSet.Config as cms
 
+import FWCore.ParameterSet.VarParsing as VarParsing
 process = cms.Process('test')
+
+options = VarParsing.VarParsing()
+supported_timetypes = {"timestamp", "lumiid"}
+options.register( 'db'
+                , 'sqlite_file:lhcinfoperfill_pop_test.db' #default value
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.string
+                , "Connection string to the DB where payloads are going to be read from"
+                  )
+options.register( 'tag'
+                , 'LHCInfoPerFill_PopCon_test'
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.string
+                , "Tag to read from in source db"
+                  )
+options.register( 'iov'
+                , 1
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.int
+                , "IOV: specifies the point in time to read the payload at"
+                  )
+options.register( 'timetype'
+                , 'timestamp' #default
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.string
+                , f"timetype of the provided IOV, accepted values: {supported_timetypes}"
+                  )
+options.parseArguments()
+
+# TODO check validity of the rest of the arguments
+if options.timetype not in supported_timetypes:
+  print(f"Provided timetype '{options.timetype}' is not supported (accepted values: {supported_timetypes})", file=sys.stderr)
+  exit(1)
 
 # minimum logging
 process.MessageLogger = cms.Service("MessageLogger",
@@ -9,20 +43,19 @@ process.MessageLogger = cms.Service("MessageLogger",
     ),
     cout = cms.untracked.PSet(
         enable = cms.untracked.bool(True),
-        threshold = cms.untracked.string('INFO')
+        threshold = cms.untracked.string('ERROR')
     )
 )
 
 process.source = cms.Source('EmptyIOVSource',
-    timetype = cms.string('runnumber'),
-    firstValue = cms.uint64(1),
-    lastValue = cms.uint64(1),
+    timetype = cms.string(options.timetype),
+    firstValue = cms.uint64(options.iov),
+    lastValue = cms.uint64(options.iov),
     interval = cms.uint64(1)
 )
-
 # load info from database
 process.load('CondCore.CondDB.CondDB_cfi')
-process.CondDB.connect = 'sqlite_file:LHCInfoPerFill.sqlite' # SQLite input
+process.CondDB.connect= options.db # SQLite input
 
 process.PoolDBESSource = cms.ESSource('PoolDBESSource',
     process.CondDB,
@@ -30,7 +63,7 @@ process.PoolDBESSource = cms.ESSource('PoolDBESSource',
     toGet = cms.VPSet(
         cms.PSet(
             record = cms.string('LHCInfoPerFillRcd'),
-            tag = cms.string('LHCInfoPerFillFake')
+            tag = cms.string(options.tag)
         )
     )
 )
