c6246fe813bc74b2161f85889ae09f0f0f8d0b46
Jan Chyczynski <jasiek.ch16@gmail.com>
2024-06-06 14:57:02 +0200

    Extract SourceHandler from LHCInfoPerFillPopConAnalyzer

--- CondTools/RunInfo/plugins/LHCInfoPerLSPopConAnalyzer.cc
+++ CondTools/RunInfo/plugins/LHCInfoPerLSPopConAnalyzer.cc
@@ -2,7 +2,7 @@
 #include "CondTools/RunInfo/interface/LHCInfoPerLSPopConSourceHandler.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-using LHCInfoPerLSPopConAnalyzer = popcon::PopConAnalyzer<LHCInfoPerLSPopConSourceHandler>;
+typedef popcon::PopConAnalyzer<LHCInfoPerLSPopConSourceHandler> LHCInfoPerLSPopConAnalyzer;
 
 DEFINE_FWK_MODULE(LHCInfoPerLSPopConAnalyzer);
 
